807901df43e9ade459c6966b7eeb0e19d36bb354
AdewoleCode <ademolaadewoleo1@gmail.com>
2023-02-10 13:16:53 +0100

    added cor

--- app.js
+++ app.js
@@ -7,15 +7,9 @@ const cors = require('cors')
 const userRouter = require('./routes/userRoutes')
 const postRouter = require('./routes/postRoutes')
 
-const corsOptions ={
-    origin:'http://localhost:3030', 
-    // origin:'https://travel-diary-backend.onrender.com/', 
-    credentials:true,  //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
 
 
-// app.use(cors())
+app.use(cors())
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
